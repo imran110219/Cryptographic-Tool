b354e7d03fcc724d83a1505b59ab499287996924
Sadman Sobhan <imran110219@gmail.com>
2020-12-28 09:38:50 +0600

    Crypto Tool

---
+++ src/main/java/com/sadman/controller/CertificateController.java
@@ -0,0 +1,24 @@
+package com.sadman.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Sadman
+ */
+public class CertificateController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void closeAction(ActionEvent event) {
+        Platform.exit();
+    }
+}
